bf9333ec56017b62e4627f717a81e91cdcec8384
fjx <fangjunxiang38@gmail.com>
2026-09-23 19:41:21 +0800

    [feat]:EXTI&TIM

--- stm32f407vgt6/Core/Src/main.c
+++ stm32f407vgt6/Core/Src/main.c
@@ -89,6 +89,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim3);
 
   /* USER CODE END 2 */
 
@@ -151,6 +152,10 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    //TODO
+}
 
 /* USER CODE END 4 */
 
